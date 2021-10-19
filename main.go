b2fc646f943a6cdd5920db95b2e4dec7cde422e0
Brett Logan <lindluni@github.com>
2021-10-18 23:20:43 -0400

    Patch dependency

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"os"
 	"strings"
 
-	"github.com/bradleyfalzon/ghinstallation"
+	"github.com/bradleyfalzon/ghinstallation/v2"
 	"github.com/google/go-github/v39/github"
 	"golang.org/x/oauth2"
 )
